e5097e5316989bb48a7c74b056780035e24e16d7
Griffon2012 <trifonov-ga@yandex.ru>
2022-11-08 00:31:30 +0300

    add task_27

---
+++ task_27/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе. Для решения задания использование цикла for является обязательным условием
+
+int GetSumNumberChar(int number)
+{
+    string numberString = Convert.ToString(Math.Abs(number));
+    int sum = 0;
+
+    for (int i = 0; i < numberString.Length; i++)
+    {
+       sum += Convert.ToInt32(numberString[i].ToString());
+    }
+
+    return sum;
+}
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"{GetSumNumberChar(number)}");
+
