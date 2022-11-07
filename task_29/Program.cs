1510aea4e0e41ee2aef2f8bad5bec34157d409f7
Griffon2012 <trifonov-ga@yandex.ru>
2022-11-08 01:02:03 +0300

    added task_29

---
+++ task_29/Program.cs
@@ -0,0 +1,27 @@
+﻿//Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран. Заполнение массива производит пользователь.
+
+string[] getFilledArray()
+{
+    string[] array = new string[8];
+    string? myString = "";
+
+    for (int i = 0; i < array.Length; i++)
+    {
+
+        Console.Write($"Введите {i + 1} элемент: ");
+        myString = Console.ReadLine();
+        if (myString is null)
+        {
+            array[i] = "";
+        }
+        else
+        {
+            array[i] = myString;
+        }
+    }
+
+    return array;
+}
+
+string[] array = getFilledArray();
+Console.WriteLine($"[{String.Join(", ", array)}]");
